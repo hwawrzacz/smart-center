096ce4d5a3f7ffd7addd98fb1baac918e4268d79
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2021-04-14 22:22:37 +0200

    Change redirection to PiHole admin panel

--- src/app/components/sidenav-menu/sidenav-menu.component.ts
+++ src/app/components/sidenav-menu/sidenav-menu.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-menu.component.scss']
 })
 export class SidenavMenuComponent implements OnInit {
-
+  private readonly PI_HOLE_ADMIN_PANEL_URL = 'http://192.168.0.2/admin';
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public openPiHoleAdminPanel(): void {
+    window.open(this.PI_HOLE_ADMIN_PANEL_URL, '_blank')
   }
 
 }
