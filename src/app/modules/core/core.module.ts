3d81399df6223d3addfedec2e9e5c02075b100f9
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2021-12-28 13:08:27 +0100

    Install flex-layout, add settings button, add active link class

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HttpClientModule } from '@angular/common/http'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule,
+  ],
+  exports: [
+    HttpClientModule,
+    FlexLayoutModule,
   ]
 })
 export class CoreModule {}
