be6b98aaf29d6cb0b2c0441e20782ac8c7926f8b
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2022-02-07 19:46:16 +0100

    Add deployment scripts

---
+++ deploy-prod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update project
+git checkout main
+git pull
+
+# Build project
+ng build --prod
+
+# Move compiled app into deployment server home directory (nginx in my case)
+sudo cp -r dist/supivisor /var/www/
+
+# Restart server service
+sudo service nginx restart
