196ec5db6d455f45ea902167e9da617aa774dffc
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2022-02-09 23:00:48 +0100

    Add last update time to dashboard-card
    Handle various mat-card-content flex-direction layouts

--- src/app/modules/shared/components/dashboard-card/dashboard-card.component.ts
+++ src/app/modules/shared/components/dashboard-card/dashboard-card.component.ts
@@ -9,6 +9,8 @@ export class DashboardCardComponent {
   @Input() title?: string
   @Input() showDetailsButton?: boolean
   @Input() showRefreshButton?: boolean
+  @Input() flexDirection: 'row' | 'column' = 'column'
+  @Input() lastUpdate?: Date
   @Output() refresh = new EventEmitter<void>()
   @Output() details = new EventEmitter<void>()
 
