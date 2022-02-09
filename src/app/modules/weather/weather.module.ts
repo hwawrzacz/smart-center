26f681109dc096ee20f51278a865cccfa8c58954
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2022-02-09 20:47:23 +0100

    Add blank weather module, component and service

---
+++ src/app/modules/weather/weather.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    WeatherCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WeatherModule { }
