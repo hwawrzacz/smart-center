70acd68618ae314fab8daaf1fba72af5d48f402d
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2022-03-06 18:05:17 +0100

    Change endpoints

--- src/app/modules/hardware-management/services/hardware-management.service.ts
+++ src/app/modules/hardware-management/services/hardware-management.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core'
 import { environment } from '../../../../environments/environment'
 import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs'
 import { HardwareStatus } from '../../../models/hardware-status'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HardwareManagementService {
-  private readonly REBOOT_URL = `${ environment.serverIpAddress }/reboot`
-  private readonly SHUTDOWN_URL = `${ environment.serverIpAddress }/shutdown`
+  private readonly REBOOT_URL = `${ environment.serverIpAddress }/manage/reboot`
+  private readonly SHUTDOWN_URL = `${ environment.serverIpAddress }/manage/shutdown`
 
   constructor(private http: HttpClient) { }
 
-  public reboot(): Observable<HardwareStatus> {
-    return this.http.get<HardwareStatus>(this.REBOOT_URL)
+  public reboot(): void {
+    this.http.get<HardwareStatus>(this.REBOOT_URL).subscribe()
   }
 
-  public shutdown(): Observable<HardwareStatus> {
-    return this.http.get<HardwareStatus>(this.SHUTDOWN_URL)
+  public shutdown(): void {
+    this.http.get<HardwareStatus>(this.SHUTDOWN_URL).subscribe()
   }
 }
